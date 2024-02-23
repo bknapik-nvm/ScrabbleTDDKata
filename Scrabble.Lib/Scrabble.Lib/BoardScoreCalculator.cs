efcaae76d6a27f1f3f01662f76940ba6ec3772eb
Bartosz Knapik <bartosz.knapik@vonage.com>
2024-02-23 16:52:39 +0100

    up to shy failing test

--- Scrabble.Lib/Scrabble.Lib/BoardScoreCalculator.cs
+++ Scrabble.Lib/Scrabble.Lib/BoardScoreCalculator.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Scrabble.Lib
 {
@@ -6,7 +8,70 @@ namespace Scrabble.Lib
     {
         public static int ScoreWord(IEnumerable<(Square Square, Tile Tile)> laidTiles, IEnumerable<Square> boardSquares)
         {
-            return 0;
+            laidTiles.ToList().ForEach(lt => {
+Console.WriteLine(lt.Square);
+Console.WriteLine("{0} {1}", lt.Tile.DisplayLetter, lt.Tile.Value);
+            });
+
+            // boardSquares.ToList().ForEach(bs => Console.WriteLine("{0} {1} {2}", bs.Point, bs.State, (bs.State as Occupied)?.Tile));
+
+            
+
+            return ScoreCurrentWord(laidTiles)+ ScorePreviousWord(boardSquares);
+
+            //  + ScoreAdjacentWords(laidTiles, boardSquares);// 
         }
+
+
+        public static int ScoreCurrentWord(IEnumerable<(Square Square, Tile Tile)> laidTiles)
+        {
+            return laidTiles.Select(lt => lt.Tile.Value).Sum() * laidTiles.Max(lt => lt.Square.Type.WordFactor);
+        }
+
+        public static Orientation GetOrientation(Square s1, Square s2)
+        {
+            if (s1.Point.X == s2.Point.X) return Orientation.Vertical;
+            if (s1.Point.Y == s2.Point.Y) return Orientation.Horizontal;
+            return Orientation.None;
+        }
+
+
+        public static int ScoreAdjacentWords(IEnumerable<(Square Square, Tile Tile)> laidTiles, IEnumerable<Square> boardSquares)
+        {
+
+            int accum = 0;
+            
+            laidTiles.ToList().ForEach(lt => {
+                var adjacent = AdjactentSquares.GetAllPossibleIntersectionSquares(boardSquares, lt.Square.Point)
+                    .Except(laidTiles.Select(l => l.Square))
+                    .Where(a => boardSquares.FirstOrDefault(bs => bs.Point.Equals(a.Point) && bs.State is Occupied) != null);
+                adjacent.ToList().ForEach(a => {
+                    accum += lt.Tile.Value;
+                    var vector = DeductPoint( a.Point, lt.Square.Point);
+                    var lastPoint = lt.Square.Point;
+                    while(true)
+                    {
+                        lastPoint = Point.Create((char) (lastPoint.X + vector.x), lastPoint.Y+vector.y);
+                        var adjacentSquareToCheck = boardSquares.First(bs => bs.Point.Equals(lastPoint));
+                        if (adjacentSquareToCheck.State is Vacant) break;
+                        accum += (adjacentSquareToCheck.State as Occupied).Tile.Value;
+                    }
+                });
+                });
+            
+            return accum;
+        }
+
+        private static (int x, int y) DeductPoint(Point p1, Point p2) {
+            return (p1.X-p2.X, p1.Y-p2.Y);
+        }
+
+        public static int ScorePreviousWord(IEnumerable<Square> boardSquares)
+        {
+            return boardSquares.Where(bs => bs.State is Occupied).Sum(bs => ((Occupied)bs.State).Tile.Value);
+        }
+
+
     }
+
 }
--- Scrabble.Lib/Scrabble.Lib/SubsequentBoardValidator.cs
+++ Scrabble.Lib/Scrabble.Lib/SubsequentBoardValidator.cs
@@ -30,11 +30,36 @@ namespace Scrabble.Lib
 
         private static bool HasIntersectingTile(IList<Square> boardSquares, Point point)
         {
-            var allPossibleIntersectionPoints = GetAllPossibleIntersectionSquares(boardSquares, point);
+            var allPossibleIntersectionPoints = AdjactentSquares.GetAllPossibleIntersectionSquares(boardSquares, point);
             return allPossibleIntersectionPoints.Any(p => p.State is Occupied);
         }
 
         private static IEnumerable<Square> GetAllPossibleIntersectionSquares(IList<Square> boardSquares, Point point)
+        {
+            var intersectingSquares = new List<Square>();
+            if (point.Y > 1)
+            {
+                intersectingSquares.Add(boardSquares.First(s => s.Point.Equals(Point.Create(point.X, point.Y - 1))));
+            }
+            if (point.Y < 15)
+            {
+                intersectingSquares.Add(boardSquares.First(s => s.Point.Equals(Point.Create(point.X, point.Y + 1))));
+            }
+            if (point.X > 'A')
+            {
+                intersectingSquares.Add(boardSquares.First(s => s.Point.Equals(Point.Create((char)(point.X - 1), point.Y))));
+            }
+            if (point.X < 'N')
+            {
+                intersectingSquares.Add(boardSquares.First(s => s.Point.Equals(Point.Create((char)(point.X + 1), point.Y))));
+            }
+            return intersectingSquares;
+        }
+    }
+
+    public class AdjactentSquares
+    {
+        public static IEnumerable<Square> GetAllPossibleIntersectionSquares(IEnumerable<Square> boardSquares, Point point)
         {
             var intersectingSquares = new List<Square>();
             if (point.Y > 1)
